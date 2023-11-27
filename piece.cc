761328e46815037053065d15d880c3be22768c06
Brian Xiao <b8xiao@uwaterloo.ca>
2023-11-27 11:55:07 -0500

    second

--- piece.cc
+++ piece.cc
@@ -1,3 +1,26 @@
 #include "piece.h"
 
-Piece::Piece(int x, int y, Colour playerColour) : x(x), y(y), playerColour(playerColour) {}
+Piece::Piece(int x, int y, Colour playerColour) 
+    : x{x}, y{x}, playerColour{playerColour}, type{type} {}
+
+int Piece::getX() const {
+    return x;
+}
+
+int Piece::getY() const {
+    return y;
+}
+
+Colour Piece::getColour() const {
+    return playerColour;
+}
+
+void Piece::setPosition(int newX, int newY) {
+    x = newX;
+    y = newY;
+}
+
+PieceType Piece::getType() const {
+    return type;
+}
+
--- piece.h
+++ piece.h
@@ -4,14 +4,26 @@
 #include <string>
 
 enum class Colour { Black, White, Green, Blue };
+enum class PieceType { King, Queen, Bishop, Knight, Rook, Pawn, NullPiece };
 
 class Piece {
     int x, y;
     Colour playerColour;
+    PieceType type;
 
   public:
     Piece(int x, int y, Colour playerColour);
     virtual ~Piece() = default;
+
+    int getX() const;
+    int getY() const;
+    void setPosition(int newX, int newY);
+    PieceType getType() const;
+    Colour getColour() const;
+
+    // Virtual methods for move validation and success
     virtual bool moveSuccess(const std::string& direction, int steps) = 0;
+    virtual bool isValidMove(int newX, int newY) const = 0;
 };
 
+#endif
