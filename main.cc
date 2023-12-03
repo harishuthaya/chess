861b22619c00c701030dcd202b698124de2fa728
Ryan Zhou <rr2zhou@uwaterloo.ca>
2023-12-03 02:56:35 -0500

    Main

--- main.cc
+++ main.cc
@@ -1,20 +1,17 @@
-#include "board.h"
-#include "textdisplay.h"
-// #include "queen.h"
 #include "game.h"
 #include <iostream>
 #include <string>
 #include <vector>
 using namespace std;
 
-// int main()
-// {
-//     string cmd;
-//     Game game;
+int main()
+{
+    string cmd;
+    Game game;
 
-//     while (true) {
-//         cin >> cmd;
-//         if (cin.eof()) break;
+    while (true) {
+        cin >> cmd;
+        if (cin.eof()) break;
 
         if (cmd == "game") {
             string p1, p2;
@@ -58,67 +55,64 @@ using namespace std;
 }
 
 
-void printBoard(Board &chessBoard)
-{
-    for (int i = 0; i < chessBoard.getSize(); i++)
-    {
-        for (int j = 0; j < chessBoard.getSize(); j++)
-        {
-            Piece *piece = chessBoard.getPiece(i, j);
-            if (piece->getType() == Type::Nullpiece)
-            {
-                cout << '_';
-            }
-            else
-            {
-                if (piece->getColour() == Colour::Black)
-                {
-                    if (piece->getType() == Type::Queen) {
-                        cout << 'q';
-                    } else if (piece->getType() == Type::Bishop) {
-                        cout << 'b';
-                    }
-                }
-                else
-                {
-                    if (piece->getType() == Type::Queen) {
-                        cout << 'Q';
-                    } else if (piece->getType() == Type::Bishop) {
-                        cout << 'B';
-                    }
-                }
-            }
-        }
-        cout << endl;
-    }
-}
+// // void printBoard(Board &chessBoard)
+// // {
+// //     for (int i = 0; i < chessBoard.getSize(); i++)
+// //     {
+// //         for (int j = 0; j < chessBoard.getSize(); j++)
+// //         {
+// //             Piece *piece = chessBoard.getPiece(i, j);
+// //             if (piece->getType() == Type::Nullpiece)
+// //             {
+// //                 cout << '_';
+// //             }
+// //             else
+// //             {
+// //                 if (piece->getColour() == Colour::Black)
+// //                 {
+// //                     if (piece->getType() == Type::Queen) {
+// //                         cout << 'q';
+// //                     } else if (piece->getType() == Type::Bishop) {
+// //                         cout << 'b';
+// //                     }
+// //                 }
+// //                 else
+// //                 {
+// //                     if (piece->getType() == Type::Queen) {
+// //                         cout << 'Q';
+// //                     } else if (piece->getType() == Type::Bishop) {
+// //                         cout << 'B';
+// //                     }
+// //                 }
+// //             }
+// //         }
+// //         cout << endl;
+// //     }
+// // }
 
-int main()
-{   
-    TextDisplay* td = new TextDisplay(8);
-    Board chessBoard(td);
-    char c;
-    while (cin >> c)
-    {
-        if (c == 'p')
-        {
-            printBoard(chessBoard);
-        }
-        else if (c == 'a')
-        {
-            char c;
-            cin >> c;
-            int a, b, d;
-            cin >> a >> b >> d;
-            chessBoard.addPiece(c, a, b, d);
-        }
-        else
-        {
-            int a, b, c, d;
-            cin >> a >> b >> c >> d;
-            bool y = chessBoard.moveSuccess(a, b, c, d);
-        }
-    }
-
-    delete td;
-}
+// int main()
+// {
+//     Board chessBoard;
+//     char c;
+//     while (cin >> c)
+//     {
+//         if (c == 'p')
+//         {
+//             printBoard(chessBoard);
+//         }
+//         else if (c == 'a')
+//         {
+//             char c;
+//             cin >> c;
+//             int a, b, d;
+//             cin >> a >> b >> d;
+//             chessBoard.addPiece(c, a, b, d);
+//         }
+//         else
+//         {
+//             int a, b, c, d;
+//             cin >> a >> b >> c >> d;
+//             bool y = chessBoard.moveSuccess(a, b, c, d);
+//         }
+//     }
+// }
